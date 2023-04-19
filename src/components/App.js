935168c19d915364e2c444d8f9b33fa076a59705
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-19 14:04:26 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import About from "./About.js";
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
 function App() {
   return (
     <div>
@@ -7,6 +9,7 @@ function App() {
       <Home />
       <About />
     </div>
+    
   );
 }
 
